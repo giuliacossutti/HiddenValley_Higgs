cd317aa8047e841696d2da9a5e42ba474a2832e5
Giulia Cossutti <giulia.cossutti@cern.ch>
2025-05-14 18:59:14 +0200

    Task19: Simulation of signal and backgrounds, pre-selection tests

---
+++ tasks/20250513_Task19/50K_bkg_ZZ.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -x
+
+echo "50K background events, diboson ZZ to charged leptons and b quarks"
+
+cd /gfsvol01/atlas/giuliac/HiggsTutorial/MG5_aMC_v3_5_6/Delphes/
+
+DelphesPythia8 /gfsvol01/atlas/giuliac/HiddenValley_Higgs/delphes_cards/delphes_card_ATLAS_Reclustering_ReclpTmin40.tcl /gfsvol01/atlas/giuliac/HiddenValley_Higgs/pythia8_cards/bkg_ZZ.cmnd  /gfsvol01/atlas/giuliac/condor/20250513_Task19/50K_bkg_ZZ.root > /gfsvol01/atlas/giuliac/condor/20250513_Task19/50K_bkg_ZZ.log
---
+++ tasks/20250513_Task19/50K_bkg_Zjets_q.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -x
+
+echo "50K background events, Z + jets from quark, Z decay to charged leptons"
+
+cd /gfsvol01/atlas/giuliac/HiggsTutorial/MG5_aMC_v3_5_6/Delphes/
+
+DelphesPythia8 /gfsvol01/atlas/giuliac/HiddenValley_Higgs/delphes_cards/delphes_card_ATLAS_Reclustering_ReclpTmin40.tcl /gfsvol01/atlas/giuliac/HiddenValley_Higgs/pythia8_cards/bkg_Zjets_q.cmnd  /gfsvol01/atlas/giuliac/condor/20250513_Task19/50K_bkg_Zjets_q.root > /gfsvol01/atlas/giuliac/condor/20250513_Task19/50K_bkg_Zjets_q.log
